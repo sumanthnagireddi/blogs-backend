252c9ed89b6beadbcf6c07aec8991527ace5b845
sumanth nagireddi <sumanthnagireddi@gmail.com>
2024-06-16 23:04:43 +0530

    i

--- connection.js
+++ connection.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose')
 
 const connect = async () => {
     try {
-        await mongoose.connect("mongodb+srv://sumanthnagireddi:2516@blogstore.m5tv4ca.mongodb.net/store")
+        await mongoose.connect(`mongodb+srv://sumanthnagireddi:2516@blogstore.m5tv4ca.mongodb.net/store?retryWrites=true&w=majority`)
         console.log("connection established");
     } catch (error) {
-        console.log("connection error1");
+        console.log(`connection error:${error}`);
     }
 }
 
